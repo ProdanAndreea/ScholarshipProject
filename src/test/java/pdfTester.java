41db5aff3ce44a3cfb2d2cc2020ef9b3e01b8129
ThisIsMeC <catalin.galben.ext@siemens.com>
2019-07-25 16:18:04 +0300

    pdf update

--- src/test/java/pdfTester.java
+++ src/test/java/pdfTester.java
@@ -1,5 +1,8 @@
 
 import com.itextpdf.io.font.FontConstants;
+import com.itextpdf.kernel.colors.Color;
+import com.itextpdf.kernel.colors.ColorConstants;
+import com.itextpdf.kernel.colors.DeviceRgb;
 import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.geom.PageSize;
@@ -10,8 +13,12 @@ import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.element.Text;
+import com.itextpdf.layout.property.TextAlignment;
 import com.itextpdf.layout.property.UnitValue;
 import org.junit.Test;
+
+import java.awt.*;
 
 
 public class pdfTester {
@@ -25,7 +32,21 @@ public class pdfTester {
             document.setMargins(20,20,20,20);
             PdfFont font = PdfFontFactory.createFont(FontConstants.TIMES_ROMAN);
 
-            Table table = new Table(new float[]{4, 2, 2, 2});
+            document.add(
+                    new Paragraph("SIEMENS SRL")
+                            .setTextAlignment(TextAlignment.LEFT)
+                            .setFontSize(22)
+                            .setBold()
+                            .setFontColor(new DeviceRgb(0,153,153))
+            );
+            document.add(
+                    new Paragraph("Bilet Invoire")
+                            .setTextAlignment(TextAlignment.CENTER)
+                            .setFontSize(20)
+                            .setBold()
+            );
+
+            Table table = new Table(new float[]{2, 2, 2, 2});
             table.setWidth(UnitValue.createPercentValue(100));
             table.addHeaderCell(
                     new Cell().add(
